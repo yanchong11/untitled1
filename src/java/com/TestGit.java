6046575f3897ecbeef5f8a25372382eee22d079b
wujinke1 <1376638160@qq.com>
2022-06-07 11:48:04 +0800

    yc

---
+++ src/java/com/TestGit.java
@@ -0,0 +1,10 @@
+package java.com;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("yanchong");
+        System.out.println("yanchong");
+        System.out.println("yanchong");
+        System.out.println("yanchong");
+    }
+}
